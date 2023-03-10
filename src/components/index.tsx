143e55dfe10e05fdb551f698ebe052e8e9cb1f8a
Gustavo Malta <gustavo.a.malta@gmail.com>
2023-03-10 14:22:02 +0000

    only backend

--- src/components/index.tsx
+++
@@ -1,7 +0,0 @@
-export { default as Button } from './Button';
-export { default as Hammer } from './Hammer';
-export { default as Input } from './Input';
-export { default as MolesGrid } from './MolesGrid';
-export { default as Mole } from './Mole';
-export { default as Background } from './Background';
-export { default as TableScore } from './TableScore';
--- src/utils/colors.tsx
+++
@@ -1,6 +0,0 @@
-export const colors = {
-  primary: '#006400',
-  secondary: '#050014',
-
-  textSecondary: '#fbf8f1',
-};
